1ce378c9e3df673da5b7851cfe5d655f033db0fa
Tatiana Tarasova <tarasova@spaziodati.eu>
2021-03-06 16:11:55 +0100

    Sticking menu to the top

--- v3/js/script.js
+++ v3/js/script.js
@@ -1,25 +1,28 @@
 const menu = document.getElementById('menu');
 const checkbox = document.getElementById('checkboxToggler');
-const checkboxWrapper = document.getElementById('checkboxWrapper');
+// const checkboxWrapper = document.getElementById('checkboxWrapper');
+const main = document.getElementById('main');
 
 function openMenu() {
     menu.style.marginRight = '0';
-    checkboxWrapper.classList.toggle('fix-position', true);
-    pageWrapper.classList.toggle('move-left', true);
+    // checkboxWrapper.classList.toggle('fix-position', true);
+    // pageWrapper.classList.toggle('move-left', true);
+    main.classList.toggle('move-left', true);
     // checkboxWrapper.style.display = "none";
+
 }
 
 function closeMenu() {
     menu.style.marginRight = '-300px';
-    checkboxWrapper.classList.toggle('fix-position', false);
-    pageWrapper.classList.toggle('move-left', false);
+    // checkboxWrapper.classList.toggle('fix-position', false);
+    // pageWrapper.classList.toggle('move-left', false);
+    main.classList.toggle('move-left', false);
     // checkboxWrapper.style.display = "initial";
 }
 
 checkbox.addEventListener('change', (event) => {
     if (event.currentTarget.checked) {
-        openMenu();
-        
+        openMenu();    
     } else {
         closeMenu();
     }
